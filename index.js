72a418fdedc855a0c9b0da0f566b77304377a2c3
Justin Hoch <justinhoch22@gmail.com>
2021-06-19 13:43:13 -0500

    .

---
+++ index.js
@@ -0,0 +1,40 @@
+const makeRequest = require("./data");
+
+let a = {};
+let b = {};
+let c = {};
+let d = {};
+let e = {};
+let f = {};
+let g = {};
+let h = {};
+let i = {};
+let j = {};
+let k = {};
+let l = {};
+let m = {};
+let n = {};
+let o = {};
+let p = {};
+let q = {};
+let r = {};
+let s = {};
+let t = {};
+let u = {};
+let v = {};
+let w = {};
+let x = {};
+let y = {};
+let z = {};
+let unvisited = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+let visited = []
+
+
+let url = "http://23.253.147.10/api/maze/"
+let urlNum = ""
+
+function apiCall() {
+    console.log(makeRequest())
+};
+
+apiCall();
