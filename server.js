a43446220bfe251533a7f295149601cd32596e67
Justin Hoch <justinhoch22@gmail.com>
2021-06-19 13:47:00 -0500

    C:/Program Files/Git/

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
